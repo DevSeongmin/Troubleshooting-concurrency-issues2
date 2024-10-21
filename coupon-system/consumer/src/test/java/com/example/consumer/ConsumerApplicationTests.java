f4d0bb2802f033b1702f0ff766dc3e96c33ef285
SeongMin Hwang <qq221qq@naver.com>
2024-10-21 21:39:13 +0900

    ""

--- coupon-system/consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
+++ coupon-system/consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.couponsystem;
+package com.example.consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CouponSystemApplicationTests {
+class ConsumerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- coupon-system/src/main/java/com/example/couponsystem/repository/CouponRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.couponsystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.couponsystem.domain.Coupon;
-
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
-
-}
